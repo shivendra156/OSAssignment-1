de095f912d10be89b1f7c20a29dd48fc66ccc21a
shivendra156 <49639640+shivendra156@users.noreply.github.com>
2019-04-19 09:59:28 +0530

    Update OSAssignment.cpp

--- OSAssignment.cpp
+++ OSAssignment.cpp
@@ -1,4 +1,5 @@
-/*#include<stdio.h>
+/*
+#include<stdio.h>
 int main()
 {
 	int tc;
@@ -16,9 +17,7 @@ int main()
 	printf("the maximum accepted fault rate for an given effective page time is  ",d);
 
 }
-*/
-/* C program to demonstrate that we can 
-ignore some string in scanf() */
+
 #include <stdio.h> 
 int main() 
 { 
@@ -29,4 +28,67 @@ int main()
 } 
 // Input : This is the value 100 
 // Output : Input value read : a = 100 
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+float pf,pfn,pm,pmn,mat,succ,success,fail,failure,et,pasr,losr,mr,fn,fi,pn;
+float n=1000000;
+cout<<"Time For A Page Fault For Empty Page :\n ";
+cin>>pf;
+pfn=pf*n;
+cout<<"Time For Modified Page :\n";
+cin>>pm;
+pmn=pm*n;
+cout<<"Enter Memory Access Time :\n";
+cin>>mat;
+cout<<"Reading Updated Page Percentage :\n";
+cin>>succ;
+success=succ/100;
+fail=100-succ;
+failure=fail/100;
+cout<<"Reading Unupdated Page Percetnage :"<<fail<<"%";
+cout<<"\nEnter The Effective Time : ";
+cin>>et;
+losr=failure*pfn;
+pasr=success*pmn;
+mr=mat+pasr+losr;
+fn=mat-mr;
+fi=mat-et;
+pn=fi/fn;
+system("cls");
+cout<<"Therefore Page Fault Related To Above Details is : ";
+cout<<"\n\n is : "<<pn<<"ns.\n";
+return 0;
+}
+/*
+Test Case 1:
+8*1000000
+20*1000000
+100
+0.7
+200
 
+TestCase 2:
+7*1000000
+10*1000000
+99
+0.7
+150
+
+Test Case 3:
+6*1000000
+100
+0.7
+150
+
+Test Case 4:
+7*1000000
+101
+0.6
+150
+*/
